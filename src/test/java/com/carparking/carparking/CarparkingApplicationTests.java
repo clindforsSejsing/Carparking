3983c151eca3e9688ebfcc9f445ba0fe2334893a
clindforsSejsing <c.lindforssejsing@gmail.com>
2022-11-16 08:55:25 +0100

    Initial commit

---
+++ src/test/java/com/carparking/carparking/CarparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carparking.carparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
